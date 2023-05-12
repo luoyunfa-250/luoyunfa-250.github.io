7adad88b46ac82b385419862886f10b7687332ed
luoyunfa-250 <132762661+luoyunfa-250@users.noreply.github.com>
2023-05-12 21:52:38 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -96,7 +96,12 @@ export default {
     content: 'Android二维码生成器',
     image: 'box6.jpeg',
     href: ''
-  }*/],
+  }*/{
+    title: '聊天室',
+    content: '晋城职业技术学院508工作室独立站点的维护',
+    image: 'box1.png',
+    href: 'https://github.com/luoyunfa-250/liaotianshi'}
+  ],
   // page6部分
   page6: {
     github: 'https://github.com/luoyunfa-250',
